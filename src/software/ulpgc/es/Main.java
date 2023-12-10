894762311bdba92466d1c5ea1487aba96f2ec876
Acoidán Rodríguez Peña <acoidanrp13@gmail.com>
2023-12-10 17:07:37 +0000

    Aplicacion acabado

---
+++ src/software/ulpgc/es/Main.java
@@ -0,0 +1,14 @@
+package software.ulpgc.es;
+
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Person> people = TsvFilePersonLoader.with("hw_25000.tsv").load();
+        Map<String,Integer> histogram = new WeightHistogramCalculator(people).calculate();
+        for (String s : histogram.keySet()) {
+            System.out.println(s + " --> " + histogram.get(s));
+        }
+    }
+}
